e3c31ecee2528b8fcb8444abf0bd04dfecebb288
root <vcherutich19@gamil.com>
2023-10-09 09:18:11 -0700

    Finding the biggest value in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        m = my_list[0]
+        for x in my_list:
+            if x > m:
+                m = x
+        return m
+    else:
+        return None
